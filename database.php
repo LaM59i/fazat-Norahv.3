4246e825de1e81bf07cbdd35081a1cba83e97b94
LaM⚜️ <125380400+LaM59i@users.noreply.github.com>
2026-05-26 20:31:03 +0300

    Update database.php

--- database.php
+++ database.php
@@ -1,12 +1,7 @@
-<?php
-$host = "sql200.infinityfree.com";
-$user = "if0_42010894";
-$pass = "aJ2dfoqghIScBjm";
-$db   = "if0_42010894_faza_db";
-
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+
+$conn->set_charset("utf8mb4");
